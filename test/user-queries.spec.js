79a7caa68b73e1e188bb21f6bcb462ceddd2e0e7
Lazhari <lazhari.mohammed@outlook.com>
2018-03-16 23:56:51 +0000

    Test the user query

--- test/user-queries.spec.js
+++ test/user-queries.spec.js
@@ -6,6 +6,7 @@ import User from '../src/models/user';
 import server from '../src';
 
 const { expect } = chai;
+const should = chai.should();
 chai.use(chaiHttp);
 
 describe('Users Queries', () => {
@@ -20,6 +21,28 @@ describe('Users Queries', () => {
                 expect(res).have.status(200);
                 expect(res.body).be.a('object');
                 expect(res.body.data.users).be.a('array');
+                return done();
+            });
+    });
+});
+
+describe('User Query', () => {
+    it('Request User by ID', () => {
+        const query = `
+            user(id: "58238f9c81164a0ba9e31617") {
+                _id
+            }
+        `;
+
+        chai
+            .request(server)
+            .post('/graphql')
+            .send({ query })
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('object');
+                res.body.data.user.should.be.a('object');
+                expect(res.body.data.user).has.property('_id');
                 return done();
             });
     });
